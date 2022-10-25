e8faec66a757ee72ee5b22764837a818e47caf5a
Many98 <e.fratrik@gmail.com>
2022-10-25 13:33:26 +0200

    updated proposal of pipeline

--- src/main.py
+++ src/main.py
@@ -6,21 +6,56 @@ from scrapers.scrape_breality import BezRealitkyScraper
 import argparse
 
 
-class Pipeline(object):
-    """class encapsulating whole preprocessing logic
+class ETL(object):
+    """class encapsulating whole preprocessing/ETL logic for data
 
-    Steps in TRAIN phase should be:
-    ----------------------
-    0. scrape Atlas cen to get prices of already sold apartments (and also fit gaussian process)
-    1. Crawl sreality/bezrealitky (regularly every week or so)
-    2. Scrape all relevant (tabular/textual) data from links provided by crawlers
-    3. Enrich records with additional features like noise levels, distance to nearest parks,
-            level of criminality nearby, estimated price from gaussian process, embeddings for textual data etc.
-    4. Feature engineering i.e.
-        a) necessary preprocessing like handling missing values, one-hot encoding, scaling features (if necessary e.g. for linear regression model) etc.
-        b) generation of additional/aggregate features (requires research of e.g. econometrial methods)
+        ?price map? | <crawl> -> [scrape] -> [enrich] -> [synchronize] -> [preprocess] -> [generate] => pd.DataFrame
+        Model generation will be independent of this ETL pipeline
 
-    5. Model generation:
+    Overall steps in ETL should be:
+    --------------------------
+    0. ?price map? optionaly scrape Atlas cen to get prices of already sold apartments (and also fit gaussian process)
+        this will be done asychronously with ETL  --> DONE (Emanuel)
+
+    1. <crawl>  crawl sreality/bezrealitky (regularly every week or so)  --> DONE (Emanuel)
+                if links are not provided as input via web app  (partially DONE) --> TODO (Hanka but research of econometrial/real estate methods is priorite)
+    2. [scrape] Scrape all relevant (tabular/textual) data from links provided by crawlers/ provided by user as url
+        Optionally process (web app) "manual" input i.e. user provides textual description and basic "tabular info"
+                    like usable area, disposition, location etc.  --> DONE (Hanka & Adam)
+    3. [synchronize] Synchronize attributes from sreality and bezrealitky data sources  --> TODO (Adam)
+    4. [enrich] Enrich records with additional features like noise levels, distance to nearest parks,
+            level of criminality nearby, estimated price from gaussian process, embeddings for textual data etc. # TODO (Emanuel)
+    5.  Feature engineering i.e.
+       [preprocess] a) necessary preprocessing like handling missing values, one-hot encoding,
+                       scaling features (if necessary e.g. for linear regression model) etc. # TODO (Emanuel)
+       [generate] b) generation of additional/aggregate features # TODO (Adama)
+                        (requires research of e.g. econometrial methods)  # TODO reserch (Hanka)
+    ---------------------------------
+
+    """
+
+    def __call__(self, *args, **kwargs):
+        # TODO here call all instances of crawlers, scrapers, enricher etc.
+        pass
+
+    def update_price_map(self):
+        """
+        Update data from atlas cen and refit gaussian process
+        Returns
+        -------
+
+        """
+        pass
+
+
+class Model(object):
+    """
+    Encapsulates model fit/predict logic on prepared data
+    [ETL] => pd.DataFrame -> [model] => prediction
+    .
+    .
+    .
+    6. [model] Independent model generation handled by class `Model` see below:
         fit final model/s
             * ideas to be tested are transform all textual data to tabular data (via embeddings to get representation) and fit probably XGboost
                 also test if providing embeddings gives non-marginal boost of prediction skill
@@ -30,21 +65,8 @@ class Pipeline(object):
                 -* or just define some query words and measure some type of distances (edit distance, dot product)
                     between every word of text and query words (probably more robust than just regexing)
 
-    Steps in INFERENCE phase should be:
-    ----------------------
-    1. Scrape all relevant (tabular/textual) data from links provided as input from app
-       OR
-       Process (web app) "manual" input i.e. user provides textual description and basic "tabular info" like usable area,
-       disposition, location etc.
-
-    2. Enrich records with additional features like noise levels, distance to nearest parks,
-            level of criminality nearby, estimated price from gaussian process, embeddings for textual data etc.
-    3. Feature engineering i.e.
-        a) necessary preprocessing like handling missing values, one-hot encoding, scaling features (if necessary e.g. for linear regression model) etc.
-        b) generation of additional/aggregate features
-
-    4. Use serialized models for prediction of price
-
+        TODO -- Hanka basic tabular model (LinearRegression, RandomForest, XGBoost)
+             -- Emanuel & Adam independent textual models/embdeddings etc
     """
 
 
