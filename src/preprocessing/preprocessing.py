2513b48c6af9a5a39c0d5f677793e18e0a0e2684
Many98 <e.fratrik@gmail.com>
2022-10-27 12:28:25 +0200

    adjusted functions for OSM data processing

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -77,6 +77,7 @@ def osmnx_call(long: float, lat: float, dist: int, tags: dict) -> gpd.GeoDataFra
     -------
 
     """
+    ox.config(use_cache=True, log_console=True)
     gdf = ox.geometries_from_point((lat, long), tags, dist)
 
     # only POLYGONS, MULTIPOLYGONS and POINTS are allowed
@@ -91,37 +92,58 @@ def osmnx_call(long: float, lat: float, dist: int, tags: dict) -> gpd.GeoDataFra
 
     values = list(itertools.chain.from_iterable(list(tags.values())))
 
+    # TODO probably remove entries with no name as we want only some popular places
     # remove nans
     gdf['geometry'].dropna(axis=0, inplace=True)
 
+    # get rid of unwanted values
     gdf.replace(np.nan, '', inplace=True)
+    for col in list(tags.keys()):
+        gdf.loc[~gdf[col].isin(values), col] = ''
 
     # merge names to one attribute
-    gdf['what'] = gdf[list(tags.keys())].agg("".join, axis=1)
+    gdf['what'] = gdf[list(tags.keys())].agg("&".join, axis=1)
 
-    # TODO remove other attributes
+    gdf.drop(list(tags.keys()), 1, inplace=True)
+
+    # replace collection of geometries like Multipolygon with multiple single geometries
+    gdf = gdf.explode(ignore_index=True)
+
+    # replace Polygons with centroids (needed for network processing) |
+    #  another point representation e.g. nearest outer node of polygon will be better but centroid is "cheaper"
+    gdf['geometry'] = gdf['geometry'].apply(lambda geo: geo.centroid if type(geo) is Polygon else geo)
 
     return gdf
 
 
-def osmnx_nearest(long: float, lat: float, dist: int) -> gpd.GeoDataFrame:
+def osmnx_nearest(long: float, lat: float, dist: int) -> pd.DataFrame:
     """
     Finds nearest features (bus station, metro, park etc) from point of interest
     Parameters
     ----------
-    long :
-    lat :
-    dist :
+    long : float
+        longitude of point of interest
+    lat : float
+        longitude of point of interest
+    dist : int
         Maximal (bbox) distance from point of interest in metres
     Returns
     -------
 
     """
-    # TODO prepare tags
-
-    tags = {'leisure': ['park'], 'amenity': ['university', 'college', 'kindergarten'],
-            'building': ['supermarket', 'hospital']}
-    tags2 = {'highway': ['bus_stop'], 'railway': ['tram_stop']}
+    # TODO prepare it to be run asynchronously / once a month update json with geometries
+    tags = {'leisure': ['park', 'dog_park', 'fitness_centre', 'playground', 'stadium',
+                        'swimming_pool', 'sports_centre', 'pitch'],
+            'amenity': ['university', 'college', 'school', 'kindergarten',
+                        'cafe', 'pub', 'restaurant',
+                        'atm', 'bank', 'post_office'
+                        'clinic', 'dentist', 'hospital', 'pharmacy',
+                        'cinema', 'theatre', 'library',
+                        'marketplace'],
+            'building': ['supermarket'],
+            'shop': ['supermarket', 'mall', 'general'],
+            'highway': ['bus_stop'], 'railway': ['tram_stop'],
+            'station': ['subway']}
 
     gdf = osmnx_call(long, lat, dist, tags)
 
@@ -129,6 +151,7 @@ def osmnx_nearest(long: float, lat: float, dist: int) -> gpd.GeoDataFrame:
     graph = ox.graph_from_point((lat, long), dist)
     #ox.plot_graph(graph)
     orig_nn = ox.nearest_nodes(graph, long, lat)  # nearest node in graph network to ours point of interest
+
     dest_nns = ox.nearest_nodes(graph, gdf['geometry'].x.values,
                                 gdf['geometry'].y.values)  # nearest nodes to all relevant geometries
     orig_nns = [orig_nn] * len(dest_nns)
@@ -137,8 +160,13 @@ def osmnx_nearest(long: float, lat: float, dist: int) -> gpd.GeoDataFrame:
     distances = [sum(x) for x in lengths]
 
     # TODO filter obtained dataset to left only nearest amenities/geometries
+    #  do we want filter only nearest because e.g. bigger park can be few metres away and still good enough (depends on preferences)
+    #  probably first 3 or so would be better --> discuss how to represent geospatial attributes except binary info and distances of nearest
+    gdf['dist'] = distances
 
-    return gdf
+    gdfc = gdf.groupby('what', as_index=False).agg({'dist': 'min', 'geometry': 'first', 'name': 'first'})
+
+    return pd.DataFrame(gdf.drop(columns='geometry'))
 
 
 def prepare_rasters(path: str) -> tuple:
