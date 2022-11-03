93fe4813c2029a156838a6336cba71aa35956040
Many98 <e.fratrik@gmail.com>
2022-11-03 22:12:11 +0100

    updated proposal of `ETL` and `Model` classes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import json
+import os
 
-from preprocessing.enrichment import Enricher
+from preprocessing.enrichment import Enricher, Generator
+from preprocessing.preprocessing import Preprocessor
+from preprocessing.synchronization import Synchronizer
 from scrapers.crawl_sreality import KindOfCrawlerForSReality
 from scrapers.crawl_breality import KindOfCrawlerForBezRealitky
 from scrapers.scrape_sreality import SRealityScraper
@@ -9,6 +13,7 @@ from scrapers.scrape_atlas_cen import AtlasCenScraper
 from models.gaussian_process import gp_train
 
 import argparse
+from typing import Union
 
 
 class ETL(object):
@@ -38,26 +43,43 @@ class ETL(object):
     ---------------------------------
 
     """
-    def __init__(self):
-        # TODO make url for crawler input
-        self.breality_crawler = KindOfCrawlerForBezRealitky(out_filename='prodej_links.txt',
-                                                       url='https://www.bezrealitky.cz/vyhledat?offerType=PRODEJ&estateType=BYT&page=1&order=TIMEORDER_DESC&regionOsmIds=R435541&osm_value=Praha%2C+%C4%8Cesko')
-        self.sreality_crawler = KindOfCrawlerForBezRealitky(out_filename='prodej_links.txt',
-                                                       url='https://www.sreality.cz/hledani/prodej/byty/praha')
+
+    def __init__(self, sreality_init_url: str = 'https://www.sreality.cz/hledani/prodej/byty/praha',
+                 breality_init_url: str = 'https://www.bezrealitky.cz/vyhledat?offerType=PRODEJ&estateType=BYT&page=1&order=TIMEORDER_DESC&regionOsmIds=R435541&osm_value=Praha%2C+%C4%8Cesko',
+                 crawled_links_filename: str = 'prodej_links.txt',
+                 scrapped_data_filename: str = 'prodej',
+                 inference: bool = False):
+
+        self.data = None
+        self.inference = inference  # whether ETL is in INFERENCE phase
+        self.crawled_links_filename = crawled_links_filename
+        self.scrapped_data_filename = scrapped_data_filename
+
+        # TODO make inits more consistent
+        self.breality_crawler = KindOfCrawlerForBezRealitky(out_filename=crawled_links_filename,
+                                                            url=breality_init_url)
+        self.sreality_crawler = KindOfCrawlerForSReality(out_filename=crawled_links_filename,
+                                                         url=sreality_init_url)
 
         self.atlas_cen_scrapper = AtlasCenScraper()
         self.breality_scraper = BezRealitkyScraper()
         self.sreality_scraper = SRealityScraper()
 
-        self.enricher = Enricher(df=pd.DataFrame())  # TODO not ideal init with emty dataframe
+        self.enricher = Enricher(df=pd.DataFrame())  # TODO not ideal init with empty dataframe
 
-    def __call__(self, links_filename: str = '', update_price_map: bool = False,  *args, **kwargs) -> pd.DataFrame:
+        # find out state of scrapped csv
+        self.scraped_init_state = self._check_state()
+        self.synchronizer = Synchronizer(from_row=self.scraped_init_state)
+
+        self.preprocessor = Preprocessor(df=pd.DataFrame())  # TODO not ideal init with empty dataframe
+
+        self.generator = Generator(df=pd.DataFrame())  # TODO not ideal init with empty dataframe
+
+    def __call__(self, update_price_map: bool = False, *args, **kwargs) -> pd.DataFrame:
         """
         call all instances of crawlers, scrapers, enricher etc.
         Parameters
         ----------
-        links_filename: str
-            Filename of .txt file with link to be scrapped. empty string means run crawlers
         update_price_map: bool
             whether to update price map
         Returns
@@ -66,46 +88,76 @@ class ETL(object):
         """
         data = None
 
-        if update_price_map:
-            pass
+        # ### CLEANING OF TEMP CSV FILES
+        self._clean()
 
-        # ### OBTAIN RAW DATA
-        if not links_filename:
-            # links firstly obtained by crawlers
+        # ### 0 UPDATE PRICE MAP
+        if update_price_map:
+            self.update_price_map()
+
+        # ### 1,2 OBTAIN RAW DATA
+        if not self.inference:
+            # links firstly obtained by crawlers and appended to `../data/prodej_links.txt`
             self.sreality_crawler.crawl()
             self.breality_crawler.crawl()
-            self.sreality_scraper.run(in_filename='prodej_links.txt', out_filename='prodej')
-            self.breality_scraper.run(in_filename='prodej_links.txt', out_filename='prodej')
-            #  ### Data are now scrapped in two separate files
-            #           ../data/prodej_breality.csv and ../data/prodej_sreality.csv so synchronization is needed
+
+            # already scrapped links are appended to `../data/already_scraped_links.txt`
+            self.sreality_scraper.run(in_filename=self.crawled_links_filename, out_filename=self.scrapped_data_filename)
+            self.breality_scraper.run(in_filename=self.crawled_links_filename, out_filename=self.scrapped_data_filename)
+            #  Data are now scrapped in two separate files
+            #           `../data/prodej_breality.csv` and `../data/prodej_sreality.csv` so synchronization is needed
             #            to get one csv with same sets of attributes
 
-            # ### SYNCHRONIZE DATA
+            # ### 3 SYNCHRONIZE DATA
+            data = self.synchronizer(
+                sreality_csv_path=f'../data/{self.scrapped_data_filename}_{self.sreality_scraper.name}_scraped.csv',
+                breality_csv_path=f'../data/{self.scrapped_data_filename}_{self.breality_scraper.name}_scraped.csv')
 
-            #  ### Data are now synchronized in one ../data/prodej_synchronized.csv (TRAIN)
-            data = pd.read_csv('../data/prodej_synchronized.csv')  # TODO dtypes specification be nice to have
+            # Data are now synchronized in one ../data/tmp_synchronized.csv (TRAIN)
+
         else:
             # input from user | used in inference
-            self.sreality_scraper.run(in_filename=links_filename, out_filename='predict')
-            self.breality_scraper.run(in_filename=links_filename, out_filename='predict')
+            # TODO web API should save input links
+            #  into `../data/predict_links.txt`
+            # TODO in future it shouldbe prepared to handle user input as text, own tabular data etc. but for now just
+            #  links
+            self.sreality_scraper.run(in_filename='predict_links.txt', out_filename='predict')
+            self.breality_scraper.run(in_filename='predict_links.txt', out_filename='predict')
             #  ### Data are now scrapped in two separate files
             #           ../data/predict_breality.csv and ../data/predict_sreality.csv so synchronization is needed
             #            to get one csv with same sets of attributes
 
-            # ### SYNCHRONIZE DATA
+            # ### 3 SYNCHRONIZE DATA
+            # TODO handle cases when empty df is returned
+            data = self.synchronizer(
+                sreality_csv_path=f'../data/predict_{self.sreality_scraper.name}_scraped.csv',
+                breality_csv_path=f'../data/predict_{self.breality_scraper.name}_scraped.csv')
 
-            #  ### Data are now synchronized in one ../data/predict_synchronized.csv (INFERENCE)
-            data = pd.read_csv('../data/predict_synchronized.csv')  # TODO dtypes specification be nice to have
+            # Data are now synchronized in one ../data/tmp_synchronized.csv (INFERENCE)
 
         if data is None:
             raise Exception('Something went wrong. Data not obtained !')
 
-        # ### ENRICH DATA
+        # ### 4 ENRICH DATA
         self.enricher.df = data  # TODO not ideal
         enriched_data = self.enricher()
+        # Data are now enriched with new geospatial attributes etc. and stored in `../data/tmp_enriched.csv`
 
-        # ### Data are now enriched with new geospatial attributes etc. now feature engineering is needed
-        pass
+        # ### 5 a PREPROCESS DATA
+        self.preprocessor.df = enriched_data  # TODO not ideal
+        preprocessed_data = self.preprocessor()
+        # Data are now preprocessed and stored in `../data/tmp_preprocessed.csv`
+
+        # ### 5 b GENERATE AGGREGATED FEATURES
+        self.generator.df = preprocessed_data  # TODO not ideal
+        final_data = self.generator()
+        # Data are now enriched with aggregated attributes and stored in `../data/tmp_final.csv`
+
+        if not self.inference:
+            self._update_state()
+            self._export_data(final_data)
+
+        return final_data
 
     def update_price_map(self):
         """
@@ -115,6 +167,66 @@ class ETL(object):
 
         """
         pass
+
+    def _export_data(self, data: pd.DataFrame) -> None:
+        """
+        method to export final dataframe to csv database
+        """
+        data.to_csv("../data/final_data.csv", mode='a', index=False, header=not os.path.exists("../data/final_data.csv"))
+
+        print(f'New data appended successfully in {"../data/final_data.csv"}!', end="\r", flush=True)
+
+    def _check_state(self) -> tuple[int, int]:
+        """
+        auxiliary method to check state of scrapped csv files to find out index of last row
+        ETL should also store own state dict in case that scraping was executed but other steps not
+        Returns
+        -------
+
+        """
+        if not os.path.isfile('../data/etl_state.json') or self.inference:
+            return 0, 0
+        else:
+            with open('../data/etl_state.json', 'r') as f:
+                state = json.load(f)
+            return state['state']
+
+    def _update_state(self):
+        """
+        auxiliary method to update state of ETL object. state is meant as index of rows to continue
+        Returns
+        -------
+
+        """
+        # TODO handle cases when no scraped csv are present
+        # TODO check if +-1 row is not needed
+        sreality_scrapped = pd.read_csv(
+            f'../data/{self.scrapped_data_filename}_{self.sreality_scraper.name}_scraped.csv')
+        breality_scrapped = pd.read_csv(
+            f'../data/{self.scrapped_data_filename}_{self.breality_scraper.name}_scraped.csv')
+
+        state = {'state': (sreality_scrapped.shape[0], breality_scrapped.shape[0])}
+
+        out = json.dumps(state, indent=4)
+
+        with open("../data/etl_state.json", "w") as outfile:
+            outfile.write(out)
+
+    def _clean(self):
+        """
+        auxiliary method to remove temp csv
+        Returns
+        -------
+
+        """
+        if os.path.isfile('../data/tmp_synchronized.csv'):
+            os.remove('../data/tmp_synchronized.csv')
+        if os.path.isfile('../data/tmp_enriched.csv'):
+            os.remove('../data/tmp_enriched.csv')
+        if os.path.isfile('../data/tmp_preprocessed.csv'):
+            os.remove('../data/tmp_preprocessed.csv')
+        if os.path.isfile('../data/tmp_final.csv'):
+            os.remove('../data/tmp_final.csv')
 
 
 class Model(object):
@@ -137,6 +249,11 @@ class Model(object):
         TODO -- Hanka basic tabular model (LinearRegression, RandomForest, XGBoost)
              -- Emanuel & Adam independent textual models/embdeddings etc
     """
+    def __init__(self, data: pd.DataFrame, inference: bool = False):
+        pass
+
+    def __call__(self, *args, **kwargs) -> Union[str, pd.DataFrame]:
+        pass
 
 
 if __name__ == "__main__":
@@ -144,22 +261,11 @@ if __name__ == "__main__":
     # parser.add_argument('-c', '--config-name', help='Name of the config file', default='config.yaml')
     # arguments = parser.parse_args()
 
-    # Here can be tested functionality of two scrapers
-    b = BezRealitkyScraper()
-    b.run(in_filename='prodej_links.txt', out_filename='prodej')
-    s = SRealityScraper()
-    s.run(in_filename='prodej_links.txt', out_filename='prodej')
-    """
-    sreality_crawler = KindOfCrawlerForSReality(out_filename='prodej_links.txt',
-                                                url='https://www.sreality.cz/hledani/prodej/byty/praha?velikost=1%2Bkk,1%2B1&stav=pred-rekonstrukci,v-rekonstrukci')
-    breality_crawler = KindOfCrawlerForBezRealitky(out_filename='prodej_links.txt',
-                                                   url='https://www.bezrealitky.cz/vyhledat?offerType=PRODEJ&estateType=BYT&page=1&order=TIMEORDER_DESC&regionOsmId=R435514&osm_value=Hlavn%C3%AD+m%C4%9Bsto+Praha%2C+Praha%2C+%C4%8Cesko')
-    sreality_crawler.crawl()
-    breality_crawler.crawl()
-    if sreality_crawler.reality_links:
-        print(f'Found {len(sreality_crawler.reality_links)} new apartments at https://www.sreality.cz/!')
-    if breality_crawler.reality_links:
-        print(f'Found {len(breality_crawler.reality_links)} new apartments ar https://www.bezrealitky.cz/!')
-    else:
-        print('No new apartments found')
-    """
+    etl = ETL(inference=False)
+    final_data = etl()
+
+    model = Model(data=final_data, inference=False)
+    # inference phase pd.DataFrame with features and predicted prices
+    # train phase will returned path to serialized trained model
+    trained = model()
+
