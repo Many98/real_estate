e9e965b7b077d179090bc68db3d431230b22e6cd
Many98 <e.fratrik@gmail.com>
2022-11-09 19:45:46 +0100

    remove unnecesary apply on gdf

--- src/preprocessing/utils.py
+++ src/preprocessing/utils.py
@@ -102,7 +102,7 @@ def osmnx_call(long: float, lat: float, dist: int, tags: dict) -> gpd.GeoDataFra
 
     # merge names to one attribute
     gdf['what'] = gdf[list(tags.keys())].agg("&".join, axis=1)
-    gdf['what'] = gdf['what'].apply(lambda x: re.sub(r'^&+|&+$', '', x))
+    #gdf['what'] = gdf['what'].apply(lambda x: '&')  not needed anymore
 
     gdf.drop(list(tags.keys()), 1, inplace=True)
 
