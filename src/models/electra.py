fde61d04e6900c698b5bed53c42ec1914c3a3340
Many98 <e.fratrik@gmail.com>
2022-11-26 21:36:24 +0100

    implemented trainig of electra, poor performance, will need to test standardization of price

--- src/models/electra.py
+++ src/models/electra.py
@@ -1,54 +1,100 @@
-from transformers import ElectraForPreTraining, ElectraTokenizerFast
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
-from utils import prepare_text_dataset
-
-# https://huggingface.co/Seznam/small-e-czech
-# https://huggingface.co/docs/transformers/training  ---> finetuning tutorial
-# https://towardsdatascience.com/how-to-fine-tune-an-nlp-regression-model-with-transformers-and-huggingface-94b2ed6f798f
-
-discriminator = ElectraForPreTraining.from_pretrained("Seznam/small-e-czech")
-tokenizer = ElectraTokenizerFast.from_pretrained("Seznam/small-e-czech")
-
-# num_labels=1 --> regression
-model = AutoModelForSequenceClassification.from_pretrained("Seznam/small-e-czech", num_labels=1)
-
-datasets = prepare_text_dataset('/home/fratrik/real_estate/data/dataset.csv')
-
-tokenized_datasets = datasets.map(lambda x: tokenizer(x["description"], padding="max_length", truncation=True),
-                                  batched=True)
-
-training_args = TrainingArguments(output_dir="test_trainer",
-                                  logging_strategy="epoch",
-                                  evaluation_strategy="epoch",
-                                  per_device_train_batch_size=8,
-                                  per_device_eval_batch_size=8,
-                                  num_train_epochs=3,
-                                  save_total_limit = 2,
-                                  save_strategy = 'no',
-                                  load_best_model_at_end=False
-                                  )
+from transformers import ElectraForPreTraining, ElectraTokenizerFast, EarlyStoppingCallback
+from transformers.integrations import TensorBoardCallback
+from torch import nn
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer,\
+    AutoFeatureExtractor
+from utils import prepare_text_dataset, compute_metrics
+import os
 
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["test"]
-)
+class RETrainer(Trainer):
+    """
+    custom Trainer
+    -- optimizer is by default AdamW
+    """
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("label", None)
+        if labels is None:
+            labels = inputs.get("labels", None)
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
 
-sentence = "Za hory, za doly, mé zlaté parohy"
-fake_sentence = "Za hory, za doly, kočka zlaté parohy"
+        print('labels', labels)
+        print('pred', logits)
+        #print('inputs', inputs)
 
-fake_sentence_tokens = ["[CLS]"] + tokenizer.tokenize(fake_sentence) + ["[SEP]"]
-fake_inputs = tokenizer.encode(fake_sentence, return_tensors="pt")
-outputs = discriminator(fake_inputs)
-predictions = torch.nn.Sigmoid()(outputs[0]).cpu().detach().numpy()
+        loss_fct = nn.MSELoss()  # or we can use nn.L1Loss()
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1, self.model.config.num_labels))
 
-for token in fake_sentence_tokens:
-    print("{:>7s}".format(token), end="")
-print()
+        return (loss, outputs) if return_outputs else loss
 
-for prediction in predictions.squeeze():
-    print("{:7.1f}".format(prediction), end="")
-print()
+    def prediction_step(
+        self,
+        model: nn.Module,
+        inputs,
+        prediction_loss_only: bool,
+        ignore_keys=None,
+    ):
+        pass
+
+
+if __name__ == '__main__':
+    # https://huggingface.co/Seznam/small-e-czech
+    # https://huggingface.co/docs/transformers/training  ---> finetuning tutorial
+    # https://towardsdatascience.com/how-to-fine-tune-an-nlp-regression-model-with-transformers-and-huggingface-94b2ed6f798f
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+    tokenizer = ElectraTokenizerFast.from_pretrained("Seznam/small-e-czech")
+
+    datasets = prepare_text_dataset('/home/fratrik/real_estate/data/dataset.csv', split_on='Kč',
+                                    use_price_m2=True, use_gp_residual=True)
+
+    datasets = datasets.rename_column('description', 'text')
+
+    tokenized_datasets = datasets.map(lambda x: tokenizer(x["text"], max_length=512, padding="max_length",
+                                                          truncation=True),
+                                      batched=True)
+
+    # num_labels=1 --> regression
+    model = AutoModelForSequenceClassification.from_pretrained("Seznam/small-e-czech", num_labels=1).to("cuda")
+
+    # to use as feature extractor
+    #feature_extractor = AutoFeatureExtractor.from_pretrained("Seznam/small-e-czech")
+
+    # https://stackoverflow.com/questions/69087044/early-stopping-in-bert-trainer-instances
+    training_args = TrainingArguments(output_dir="test_trainer",
+                                      logging_strategy="epoch",
+                                      logging_dir='logs',
+                                      evaluation_strategy="epoch",
+                                      #eval_steps=200,
+                                      #save_steps=200,
+                                      learning_rate=2e-1,
+                                      weight_decay=0.001,
+                                      per_device_train_batch_size=32,
+                                      per_device_eval_batch_size=32,
+                                      num_train_epochs=100,
+                                      save_total_limit=5,
+                                      save_strategy='epoch',
+                                      metric_for_best_model='mae',
+                                      load_best_model_at_end=True
+                                      )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["test"],
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=10), TensorBoardCallback()]
+    )
+
+    trainer.train()
+
+    print('trained')
+
+    # TODO at model.eval() stage predictions are constant WTF
+    # TODO try train on `price_m2`
+    # TODO try train on residual price/price_m2 from XGB/GP/TargetEncoding
+    # TODO use standardization !!!
