2e63540f8314e8880d7464a48fe56c0027b4215d
Many98 <e.fratrik@gmail.com>
2022-12-01 21:55:00 +0100

    added tuning of `XGBQuantile`

--- src/models/xgb_tuning.py
+++ src/models/xgb_tuning.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from xgboost import XGBRegressor
+from models.XGBQuantile import XGBQuantile
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
+import scipy
 
 
 def xgb_tune(X, y, njobs=5):
@@ -32,5 +34,38 @@ def xgb_tune(X, y, njobs=5):
     return random_search.best_estimator_
 
 
-def xgb_quantile_tune(df: pd.DataFrame):
-    pass
+def xgb_quantile_tune(X, y, alpha, njobs=5):
+    params = {
+         'quant_delta': scipy.stats.uniform(0.01, 10.0),
+         'quant_var': scipy.stats.uniform(1.0, 10.0),
+         'quant_thres': scipy.stats.uniform(0.01, 10.0)
+
+    }
+
+    reg = XGBQuantile(
+                    quant_alpha=alpha,
+                    #quant_delta= 1.0,
+                    #quant_thres = 1.0 ,quant_var=1.0,
+                    n_estimators=800,
+                    learning_rate=0.05,
+                    colsample_bytree=1.0,
+                    colsample_bynode=0.60,
+                    # objective='reg:squarederror',
+                    # eval_metric=mean_absolute_error,
+                    max_depth=8,
+                    tree_method='hist',
+                    # enable_categorical=True,
+                    subsample=0.8,
+                    random_state=42,
+                    # silent=True,
+                    booster='dart',
+                    n_jobs=njobs
+                )
+    n_iter_search = 100
+    random_search = RandomizedSearchCV(reg, param_distributions=params, n_iter=n_iter_search, cv=5, n_jobs=njobs,
+                                        # 'neg_mean_absolute_error',
+                                       verbose=1)
+
+    random_search.fit(X, y)
+
+    return random_search.best_estimator_
