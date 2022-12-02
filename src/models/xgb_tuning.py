ad212ecae65ff81207f0e738b917a3f7b55fcb5b
Many98 <e.fratrik@gmail.com>
2022-12-02 21:13:24 +0100

    adjusted tuning

--- src/models/xgb_tuning.py
+++ src/models/xgb_tuning.py
@@ -8,25 +8,23 @@ import scipy
 
 def xgb_tune(X, y, njobs=5):
     params = {
-        'max_leaves': [0, 10, 20, 50, 100],
-        'max_depth': [2, 3, 4, 5, 6, 7, 10, 15],
-        'learning_rate': [0.0001, 0.001, 0.01, 0.05, 0.08999, 0.1, 0.11, 0.12, 0.15, 0.25, 0.3],
+        'max_leaves': [0, 50, 100],
+        'max_depth': [3, 4, 5, 6, 7, 10, 15],
+        'learning_rate': [0.01, 0.05, 0.08999, 0.1, 0.11, 0.12, 0.15, 0.25],
         'n_estimators': [i * 100 for i in range(1, 10)],
         'gamma': [0, 0.1, 1, 10, 100],
         'subsample': [i / 10.0 for i in range(6, 11)],
         'colsample_bytree': [i / 10.0 for i in range(6, 11)],
         'colsample_bynode': [i / 10.0 for i in range(6, 11)],
-        'booster': ['gbtree', 'dart', 'gblinear'],
-        #'eval_metric': ['rmse'],
-        #'tree_method': ['approx', 'hist'],
+        'booster': ['gbtree', 'dart'],
     }
     reg = XGBRegressor(nthread=None, njobs=None,
                        objective='reg:squarederror',
                        tree_method='hist',
                        random_state=42)
-    n_iter_search = 5000
+    n_iter_search = 1000
     random_search = RandomizedSearchCV(reg, param_distributions=params, n_iter=n_iter_search, cv=5, n_jobs=njobs,
-                                       scoring='r2', #'neg_mean_absolute_error',
+                                       scoring='neg_mean_absolute_error',
                                        verbose=1)
 
     random_search.fit(X, y)
